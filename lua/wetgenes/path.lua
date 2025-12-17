acf6f70182e1338ca868f9ba6b92bbeab4efa99f
kriss <kriss@xixs.com>
2025-12-17 17:47:39 +0000

    fix double .. paths

--- lua/wetgenes/path.lua
+++ lua/wetgenes/path.lua
@@ -226,9 +226,13 @@ wpath.normalize=function(p)
 			table.remove(ps,idx)
 		elseif ps[idx]==".." then -- remove this and the previous one if we can
 			if idx>(pp.root and 2 or 1) then -- can we remove previous part
-				idx=idx-1
-				table.remove(ps,idx)
-				table.remove(ps,idx)
+				if ps[idx-1]==".." then -- double .. cant remove
+					idx=idx+1
+				else
+					idx=idx-1
+					table.remove(ps,idx)
+					table.remove(ps,idx)
+				end
 			else -- we can not remove so must ignore
 --				table.remove(ps,idx)
 				idx=idx+1
